6ffb41b45ffc84e484b074cd3da54a650629a093
vuduyviet1110 <2001040225@s.hanu.edu.vn>
2024-06-02 19:18:07 +0700

    implement almost feature

--- src/components/forms/PostThread.tsx
+++ src/components/forms/PostThread.tsx
@@ -16,7 +16,6 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
 
