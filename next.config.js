bb2f5c755879a2d089bc746fd8cc0ab593758229
vuduyviet1110 <2001040225@s.hanu.edu.vn>
2024-06-02 14:14:21 +0700

    bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescrip: {
-    ingnoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
   experimental: {
     serverActions: true,
